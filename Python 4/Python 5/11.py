e7fd1a3b5316e40fbd1a58b40e8055c9245f6cf4
M.A.S.V.Karunathilaka <144529370+SahanVkaru@users.noreply.github.com>
2026-02-10 14:59:35 +0530

    Add Python 4.zip and update 11.py
    Adds a new binary archive Python 4.zip and appends a test/debug snippet to Python 4/Python 5/11.py. The appended code sets n=6 and attempts to print a triangle of '*' characters but contains early break statements and duplicated nested loops (likely leftover debug code). Note: file ends without a trailing newline.

--- Python 4/Python 5/11.py
+++ Python 4/Python 5/11.py
@@ -9,3 +9,16 @@ for i in range(n, 0, -1):
     for j in range(i):
         print('* ', end="")
     print('')
+
+
+n=6
+for i in range(n):
+    for j in range(i):
+        print('* ', end="")
+    print('')
+    break
+    for i in range(n):
+        for j in range(i):
+            print('* ', end="")
+        print('')
+        break
